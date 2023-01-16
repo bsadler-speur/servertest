454bee7044885fa7c552477cb9d5e96de05bee41
unknown <brendon.sadler@speurgroup.com>
2023-01-16 16:07:00 -0500

    test server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080
+
+
+app.listen(PORT,() =>{
+    console.log("Server is running")
+});
+
+app.post("/submit",(req,res)=>{
+    console.log(res)
+})
